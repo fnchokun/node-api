5f573ca796f19e445baaa7ebdac5df6bea694887
fnchokun <fnchokun@gmail.com>
2023-07-28 12:31:49 +0700

    to vercel

--- index.js
+++ index.js
@@ -4,20 +4,26 @@ const app = express()
 
 const PORT = process.env.PORT || 8080;
 
-app.use(cors())
+app.use(express.static('public'))
 
-app.get('/', function (req, res, next) {
-    res.json({
-        message: `Hello World!`
-    })
+
+app.get('/', function (req, res) {
+    // res.json({
+    //     message: `Hello World!`
+    // })
+
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+
 })
 
-app.get('/ping', function (req, res, next) {
-    res.json({
-        message: `Pong!`
-    })
-})
+// app.get('/ping', function (req, res, next) {
+//     res.json({
+//         message: `Pong!`
+//     })
+// })
 
 app.listen(PORT, function () {
     console.log(`CORS-enabled web server listening on port ${PORT}`)
 })
+
+module.exports = app
