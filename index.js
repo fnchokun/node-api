8ceada37b7eea3669f8252c32a889f00a3f03ee7
fnchokun <fnchokun@gmail.com>
2023-07-28 12:16:57 +0700

    initialize

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+const PORT = process.env.PORT || 8080;
+
+app.use(cors())
+
+app.get('/', function (req, res, next) {
+    res.json({
+        message: `Hello World!`
+    })
+})
+
+app.get('/ping', function (req, res, next) {
+    res.json({
+        message: `Pong!`
+    })
+})
+
+app.listen(PORT, function () {
+    console.log(`CORS-enabled web server listening on port ${PORT}`)
+})
